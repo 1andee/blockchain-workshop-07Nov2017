418d2d106c4e58c563ffae265550cf9ee2e60487
Ian Duke <ian@thefarmventures.com>
2017-11-07 19:53:27 -0500

    ran truffle build

--- truffle.js
+++ truffle.js
@@ -1,4 +1,10 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "blkchnbvruvo.eastus.cloudapp.azure.com", // rpc endpoint
+      port: 8545,
+      network_id: "*",
+      gas: 400000
+    }
+  }
 };
