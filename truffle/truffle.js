495a18ccd6bf231ca94e6a03dcdf158a1df6e8a9
Ian Duke <ian@thefarmventures.com>
2017-11-07 19:57:42 -0500

    fix folder nesting

---
+++ truffle/truffle.js
@@ -0,0 +1,10 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "blkchnbvruvo.eastus.cloudapp.azure.com", // rpc endpoint
+      port: 8545,
+      network_id: "*",
+      gas: 400000
+    }
+  }
+};
